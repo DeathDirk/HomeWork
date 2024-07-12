94b7e912c8025c2299d291455bc63c10e9039d37
DeathDirk <dirkoflife@gmail.com>
2024-07-12 11:33:08 +0300

    1

--- Homework3.py
+++ Homework3.py
@@ -2,7 +2,7 @@ name = 'Victor'
 print('Name: ' + name)
 age = 36
 print('Age: ' + str(age))
-age = 37
-print('New Age: ' + str(age))
+new_age = age + 1
+print('New Age: ' + str(new_age))
 is_student = True
 print('Is Student: ' + str(is_student))
